a398fdba762b0c7f7e7341a5aa68eab52c2123ae
Lmao1232j <vinhtran23042004@gmail.com>
2025-03-30 18:00:20 +0700

    Finish landing page

---
+++ JavaScript/home.js
@@ -0,0 +1,28 @@
+function preload_image(im_url) {
+    let img = new Image();
+    img.src = im_url;
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const left_slideshow = document.querySelectorAll(".slideshow-title");
+    const slideshow = document.getElementsByClassName("left-slideshow-content");
+    const img = document.querySelectorAll(".left-slideshow-content img");
+
+    left_slideshow.forEach(function(i) {
+        i.addEventListener("click", function() {
+            for (let y = 0; y < 4; y++) {
+                if (left_slideshow[y] == i) {
+                    i.style.color = "#d3dde7";
+                    slideshow[y].style.display = "block";
+                    continue;
+                }
+                if (left_slideshow[y].style.color != "black") {
+                    left_slideshow[y].style.color = "black";
+                    slideshow[y].style.display = "none";
+                    preload_image(img[y].src);
+                }
+            }
+        });
+    });
+});
+
