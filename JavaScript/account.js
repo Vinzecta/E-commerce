38cc6ed5039030d89edb55108253ec51fea06fe8
Lmao1232j <vinhtran23042004@gmail.com>
2025-04-05 00:17:49 +0700

    Finish account page

--- JavaScript/account.js
+++ JavaScript/account.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const forgot = document.getElementById("forgot-password");
     const forgot_password = document.getElementById("forgot");
     const form = document.getElementsByTagName("form");
+    const user_image = document.getElementById("user-image");
+    const file_upload = document.getElementById("file-upload");
+    const profile = document.getElementById("my-account");
+    const purchase = document.getElementById("purchase-history");
+    const reset_password = document.getElementById("reset-password");
+    const hover_list = document.querySelectorAll(".list-container");
+    const right_content = document.querySelectorAll(".right-content");
 
     function reset_input(input) {
         for (let i = 0; i < input.length; i++) {
@@ -72,6 +79,68 @@ document.addEventListener("DOMContentLoaded", function() {
     forgot_password.addEventListener("click", function() {
         forgot.style.display = "flex";
         loginform.style.display = "none";
+        option.style.display = "none";
         reset_input(form);
+    });
+    
+    file_upload.addEventListener("change", function() {
+        const reader = new FileReader();
+        reader.onload = function () {
+            user_image.src = reader.result;
+        };
+        if (file_upload.files[0]) {
+            reader.readAsDataURL(file_upload.files[0]);
+        }
+    });
+
+    profile.addEventListener("click", function() {
+        let a = 0;
+        for (let i = 0; i < hover_list.length; i++) {
+            if (i === a) {
+                continue;
+            }
+            if (hover_list[i].style.backgroundColor != "white") {
+                hover_list[i].style.backgroundColor = "white";
+            }
+            if (right_content[i].style.display != "none") {
+                right_content[i].style.display = "none";
+            }
+        }
+        right_content[a].style.display = "flex";
+        hover_list[a].style.backgroundColor = "#f2f2f2";
+    });
+
+    purchase.addEventListener("click", function() {
+        let a = 1;
+        for (let i = 0; i < hover_list.length; i++) {
+            if (i === a) {
+                continue;
+            }
+            if (hover_list[i].style.backgroundColor != "white") {
+                hover_list[i].style.backgroundColor = "white";
+            }
+            if (right_content[i].style.display != "none") {
+                right_content[i].style.display = "none";
+            }
+        }
+        right_content[a].style.display = "flex";
+        hover_list[a].style.backgroundColor = "#f2f2f2";
+    });
+
+    reset_password.addEventListener("click", function() {
+        let a = 2;
+        for (let i = 0; i < hover_list.length; i++) {
+            if (i === a) {
+                continue;
+            }
+            if (hover_list[i].style.backgroundColor != "white") {
+                hover_list[i].style.backgroundColor = "white";
+            }
+            if (right_content[i].style.display != "none") {
+                right_content[i].style.display = "none";
+            }
+        }
+        right_content[a].style.display = "flex";
+        hover_list[a].style.backgroundColor = "#f2f2f2";
     });
 });
--- Web pages/account.php
+++ Web pages/account.php
@@ -17,12 +17,97 @@
 
 `   <h1 id="title">Account</h1>
 
+    <section id="profile">
+        <div id="left-profile">
+            <div class="list-container" id="my-account">
+                <div class="profile-pic">
+                    <img src="../Images/Header/user.png">
+                </div>
+                <p>My Profile</p>
+            </div>
+
+            <div class="list-container" id="purchase-history">
+                <div class="profile-pic">
+                    <img src="../Images/Header/user.png">
+                </div>
+                <p>Purchase History</p>
+            </div>
+
+            <div class="list-container" id="reset-password">
+                <div class="profile-pic">
+                    <img src="../Images/Header/user.png">
+                </div>
+                <p>Reset Password</p>
+            </div>
+        </div>
+
+        <div id="right-profile">
+            <div class="right-content" id="right-account">
+                <div id="right-account-title">
+                    <h1>My account</h1>
+                </div>
+                <form id="edit-profile">
+                    <div id="image-edit">
+                        <img id="user-image" src="../Images/Account/user.png" alt="user">
+                        <input id="file-upload" type="file" accept=".jpg,.png">
+                    </div>
+                    <label>Username</label>
+                    <input type="text" value="Vinzecta" disabled readonly>
+                    <p style="text-align: center; padding: 0">You cannot change the username</p>
+                    <label>Name</label>
+                    <input type="text" placeholder="Enter name">
+                    <label>Email</label>
+                    <input type="text" value="lmao@gmail.com">
+                    <label>Phone number</label>
+                    <input type="text" placeholder="Enter phone number">
+                    <label>Gender</label>
+                    <div id="gender">
+                        <input type="radio">
+                        <label>Male</label>
+                        <input type="radio">
+                        <label>Female</label>
+                        <input type="radio">
+                        <label>Other</label>
+                    </div>
+                    <label>Date of birth</label>
+                    <input type="date">
+                    <button class="change-profile" type="submit">Save changes</button>
+                </form>
+            </div>
+
+            <div class="right-content" id="right-history" style="display: none">
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th>Purchase date</th>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="right-content" id="right-reset" style="display: none">
+                <div id="right-reset-title">
+                    <h1>Reset password</h1>
+                </div>
+                <form id="reset-pass">
+                    <label>New password</label>
+                    <input type="text" placeholder="Enter new password">
+                    <label>Re-type password</label>
+                    <input type="text" placeholder="Re-type password">
+                    <button class="change-profile" type="submit">Save changes</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
     <section id="credential">
-            <div id="option">
+            <div id="option" style="display: none">
                 <p id="login">Log in</p>
                 <p id="signup">Sign up</p>
             </div>
-            <form id="sign-in" class="credential-form">
+            <form id="sign-in" class="credential-form" style="display: none">
                 <label for="username">Username or E-mail address</label>
                 <input type="text" class="username" placeholder="Enter username / Email address">
                 <label for="password">Password</label>
@@ -32,10 +117,10 @@
                     <p>Show password</p>
                 </div>
                 <p id="forgot">Forgot password?</p>
-                <a href="index.php?page=home">Log in</a>
+                <a href="index.php?page=home">Sign in</a>
             </form>
 
-            <form id="sign-up" class="credential-form">
+            <form id="sign-up" class="credential-form" style="display: none">
                 <label for="username">Username</label>
                 <input type="text" class="username" placeholder="Enter username">
                 <label for="email">Email</label>
@@ -51,7 +136,7 @@
                 <a href="index.php?page=home">Sign up</a>
             </form>
 
-            <form id="forgot-password" class="credential-form">
+            <form id="forgot-password" class="credential-form" style="display: none">
                 <div id="instruction">
                     <h2>Forget your password?</h2>
                     <p>Type your email and new password!</p>
@@ -66,7 +151,7 @@
                     <input type="checkbox">
                     <p>Show password</p>
                 </div>
-                <a href="index.php?page=account">Sign up</a>
+                <a href="index.php?page=account">Submit</a>
                 <div id="return-section">
                     <p>Remember your password?</p>
                     <p id="login-return">Return to login</p>
@@ -78,5 +163,8 @@
         include "./Components/info.php";
         include "./Components/footer.php";
     ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/js/bootstrap.min.js" integrity="sha384-Re460s1NeyAhufAM5JwfIGWosokaQ7CH15ti6W5Y4wC/m4eJ5opJ2ivohxVM05Wd" crossorigin="anonymous"></script>
 </body>
 </html>
